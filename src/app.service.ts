3b66bdaba1bfaf0e49ccdcfbe18d40c0ad0ec2dc
Eduards-ai <eduards.sniedze@gmail.com>
2024-08-04 19:26:27 +0300

    test: check methods in fabric, check futureBuy

--- src/app.service.ts
+++ src/app.service.ts
@@ -66,29 +66,29 @@ export class AppService {
       const redisBalance: string = await redisInstance.get(
         redisInstance.generateRedisKey({
           key: 'balance',
-          marketName: marketLow,
+          marketName: marketHigh,
           balanceType: 'spot',
           asset: 'usdt',
         }),
       );
-      const low: Market = new this.markets[marketLow].factory().getMarket(
-        amountToBuy,
-        asset,
-        aproxStableValue,
-        this.markets[marketLow].service,
-      );
-
-      // const high: Market = new this.markets[marketHigh].factory().getMarket(
+      // const low: Market = new this.markets[marketLow].factory().getMarket(
       //   amountToBuy,
       //   asset,
       //   aproxStableValue,
-      //   this.markets[marketHigh].service,
+      //   this.markets[marketLow].service,
       // );
+
+      const high: Market = new this.markets[marketHigh].factory().getMarket(
+        amountToBuy,
+        asset,
+        aproxStableValue,
+        this.markets[marketHigh].service,
+      );
       if (Number(redisBalance)) {
-        // low.walletTransfer(redisBalance);
-        // low.check();
+        // high.walletTransfer(redisBalance);
+        // high.check();
         // low.futureWalletTransfer();
-        // low.checkFuture();
+        high.checkFuture();
         // low.check();
         // low.buy();
         // setTimeout(() => {
@@ -97,317 +97,149 @@ export class AppService {
         // setTimeout(() => {
         //   low.check();
         // }, 200);
-        // low.futureBuy();
+        // high.futureBuy();
         // setTimeout(() => {
-        //   low.futureSell();
+        // high.futureSell();
         // });
-        //   if (Number(redisBalance) > 30) {
-        //     const spotResult = await this.markets[marketLow]['buy'](
-        //       marketLow == 'binance' ? redisBalance : amountToBuy,
-        //       asset,
-        //       aproxStableValue,
-        //     );
-        //     await this.setTransactionRedis({
-        //       externalTransactionId: spotResult?.txid,
-        //       market: marketLow,
-        //       amountToBuy,
-        //       price: spotResult?.result?.price,
-        //       asset,
-        //       status: spotResult?.result?.status,
-        //       type: ActionType.SPOT_BUY,
-        //       balanceType: 'spot',
-        //       value:
-        //         Number(redisBalance) -
-        //         (Number(spotResult?.result?.cost) +
-        //           Number(spotResult?.result?.fee)),
-        //     });
-        //     if (marketLow === 'kraken') {
-        //       const transferResult = await this.markets[marketLow][
-        //         'walletTransfer'
-        //       ]('usdt', redisBalance, KrakenWallets.SPOT, KrakenWallets.FUTURES);
-        //       if (transferResult.data?.result?.refid) {
-        //         await redisInstance.set(
-        //           {
+        //     if (Number(redisBalance) > 30) {
+        //       const spotResult = await this.markets[marketLow]['buy'](
+        //         marketLow == 'binance' ? redisBalance : amountToBuy,
+        //         asset,
+        //         aproxStableValue,
+        //       );
+        //       //binance spot buy to redis
+        //       await this.setTransactionRedis({
+        //         externalTransactionId: spotResult.orderId,
+        //         market: marketLow,
+        //         amountToBuy: spotResult.origQty,
+        //         price: spotResult.fills[0].price,
+        //         asset: spotResult.symbol,
+        //         status: spotResult.status,
+        //         type: ActionType.SPOT_BUY,
+        //         balanceType: 'spot',
+        //         value: Number(redisBalance) - Number(spotResult?.cummulativeQuoteQty),
+        //       });
+        //       // biance spot sell
+        //       await this.setTransactionRedis(
+        //         await this.markets[marketLow]['sell']('ETH', redisBalance),
+        //       );
+        //       try {
+        //         let redisFutureBalance: string = await redisInstance.get(
+        //           redisInstance.generateRedisKey({
         //             key: 'balance',
-        //             marketName: marketLow,
-        //             balanceType: 'spot',
-        //             asset: 'usdt',
-        //             value: '0',
-        //           },
-        //           300,
+        //             marketName: marketHigh,
+        //             balanceType: 'futures',
+        //             asset: 'bnfcr',
+        //           }),
         //         );
-        //         await this.setTransactionRedis({
-        //           externalTransactionId: transferResult.data?.result?.refid,
-        //           market: marketLow,
-        //           amountToBuy: redisBalance,
-        //           price: 'undefined',
-        //           asset: 'usdt',
-        //           status: 'success',
-        //           type: ActionType.TRANSFER,
-        //           balanceType: 'spot',
-        //           value: redisBalance,
-        //         });
-        //         const futureResult = await this.markets[marketHigh]['futureBuy'](
-        //           amountToBuy,
-        //           asset,
+        //         // binace future buy
+        //         await this.setTransactionRedis(
+        //           await this.markets[marketHigh].futureBuy(
+        //             Number(spotResult.origQty).toFixed(3),
+        //             asset,
+        //             redisFutureBalance,
+        //           ),
         //         );
+        //         // binance future sell
+        //         await this.setTransactionRedis(
+        //           await this.markets[marketHigh].futureSell(
+        //             asset,
+        //             redisFutureBalance,
+        //           ),
+        //         );
+        //       } catch (error) {
+        //         log(error);
+        //       }
+        //     }
+        //   } catch (error) {
+        //     log(error);
+        //   }
+        //   try {
+        //     const redisBalance: string = await redisInstance.get(
+        //       redisInstance.generateRedisKey({
+        //         key: 'balance',
+        //         marketName: marketHigh,
+        //         balanceType: 'spot',
+        //         asset: 'usdt',
+        //       }),
+        //     );
+        //     if (marketHigh == 'binance') {
+        //       const futureBuyResult = await this.markets[
+        //         marketHigh
+        //       ].service.futureBuy(redisBalance, asset, aproxStableValue);
+        //       if (futureBuyResult) {
+        //         const remainingBalance = futureBuyResult.remainingBalance;
+        //         const result = futureBuyResult.futureBuyResponse;
         //         await this.setTransactionRedis({
-        //           externalTransactionId: futureResult?.sendStatus?.order_id,
-        //           market: marketLow,
-        //           amountToBuy,
-        //           price: futureResult?.sendStatus?.orderEvents[0]?.price,
-        //           asset:
-        //             futureResult?.sendStatus?.orderEvents[0]?.orderPriorExecution
-        //               ?.symbol,
-        //           status: futureResult?.sendStatus?.status,
+        //           externalTransactionId: result.orderId,
+        //           market: marketHigh,
+        //           amountToBuy: result.origQty,
+        //           price: Number(redisBalance) - Number(remainingBalance),
+        //           asset: result.symbol,
+        //           status: result.status,
         //           type: ActionType.FUTURE_BUY,
-        //           balanceType: 'futures',
-        //           value: Number(futureResult?.sendStatus?.orderEvents[0]?.amount),
+        //           balanceType: 'spot',
+        //           value: remainingBalance,
         //         });
+        //         console.log(await redisInstance.get('balance-binance-spot-usdt'));
         //       }
-        //       const futureSellResult = await this.markets[marketLow]['futureSell'](
-        //         amountToBuy,
-        //         asset,
-        //       );
-        //       await this.setTransactionRedis({
-        //         externalTransactionId: futureSellResult?.sendStatus?.order_id,
-        //         market: marketLow,
-        //         amountToBuy: futureSellResult?.sendStatus?.orderEvents[0]?.amount,
-        //         price: futureSellResult?.sendStatus?.orderEvents[0]?.price,
-        //         asset:
-        //           futureSellResult?.sendStatus?.orderEvents[0]?.orderPriorExecution
-        //             ?.symbol,
-        //         status: futureSellResult?.sendStatus?.status,
-        //         type: ActionType.FUTURE_SELL,
-        //         balanceType: 'futures',
-        //         value: Number(futureSellResult?.sendStatus?.orderEvents[0]?.amount),
-        //       });
-        //       await this.markets[marketLow]['checkFuture'](asset).then(
-        //         (response) => {
-        //           if (response) {
-        //             redisBalance = response[asset];
-        //           }
-        //         },
-        //       );
-        //       const futureBalance: string = await redisInstance.get(
-        //         redisInstance.generateRedisKey({
-        //           key: 'balance',
-        //           marketName: marketLow,
-        //           balanceType: 'futures',
-        //           asset: 'usdt',
-        //         }),
-        //       );
-        //       const transferBackToSpot = await this.markets[marketLow][
-        //         'futureWalletTransfer'
-        //       ]('usdt', futureBalance);
-        //       if (transferBackToSpot) {
-        //         await redisInstance.set(
-        //           {
-        //             key: 'balance',
-        //             marketName: marketLow,
-        //             balanceType: 'spot',
-        //             asset: 'futures',
-        //             value: redisBalance,
-        //           },
-        //           300,
-        //         );
-        //         await this.setTransactionRedis({
-        //           externalTransactionId: transferBackToSpot?.data?.result?.refid,
-        //           market: marketLow,
-        //           amountToBuy: redisBalance,
-        //           price: 'undefined',
-        //           asset: 'usdt',
-        //           status: 'success',
-        //           type: ActionType.TRANSFER,
-        //           balanceType: 'futures',
-        //           value: redisBalance,
-        //         });
-        //       }
-        //       const sellResult = await this.markets[marketHigh]['sell'](
-        //         amountToBuy,
-        //         asset.toUpperCase(),
-        //       );
-        //       await this.setTransactionRedis({
-        //         externalTransactionId: sellResult.txid,
-        //         market: marketLow,
-        //         amountToBuy: spotResult.amount[0],
-        //         price: spotResult.result[spotResult.txid].price,
-        //         asset,
-        //         status: spotResult.result[spotResult.txid].status,
-        //         type: ActionType.SPOT_SELL,
-        //         balanceType: 'spot',
-        //         value: spotResult.amount[0],
-        //       });
         //     }
-        //     //binance spot buy to redis
-        //     await this.setTransactionRedis({
-        //       externalTransactionId: spotResult.orderId,
-        //       market: marketLow,
-        //       amountToBuy: spotResult.origQty,
-        //       price: spotResult.fills[0].price,
-        //       asset: spotResult.symbol,
-        //       status: spotResult.status,
-        //       type: ActionType.SPOT_BUY,
-        //       balanceType: 'spot',
-        //       value: Number(redisBalance) - Number(spotResult?.cummulativeQuoteQty),
-        //     });
-        //     // biance spot sell
-        //     await this.setTransactionRedis(
-        //       await this.markets[marketLow]['sell']('ETH', redisBalance),
-        //     );
-        //     try {
-        //       let redisFutureBalance: string = await redisInstance.get(
-        //         redisInstance.generateRedisKey({
-        //           key: 'balance',
-        //           marketName: marketHigh,
-        //           balanceType: 'futures',
-        //           asset: 'bnfcr',
-        //         }),
-        //       );
-        //       // binace future buy
-        //       await this.setTransactionRedis(
-        //         await this.markets[marketHigh].futureBuy(
-        //           Number(spotResult.origQty).toFixed(3),
-        //           asset,
-        //           redisFutureBalance,
-        //         ),
-        //       );
-        //       // binance future sell
-        //       await this.setTransactionRedis(
-        //         await this.markets[marketHigh].futureSell(
-        //           asset,
-        //           redisFutureBalance,
-        //         ),
-        //       );
-        //     } catch (error) {
-        //       log(error);
-        //     }
+        //   } catch (error) {
+        //     log(error);
         //   }
-        // } catch (error) {
-        //   log(error);
-        // }
-        // try {
-        //   const redisBalance: string = await redisInstance.get(
-        //     redisInstance.generateRedisKey({
-        //       key: 'balance',
-        //       marketName: marketHigh,
-        //       balanceType: 'spot',
-        //       asset: 'usdt',
-        //     }),
+        //   await this.markets[marketHigh]['futureBuy'](
+        //     amountToBuy,
+        //     asset,
+        //     aproxStableValue,
         //   );
-        //   if (marketHigh == 'binance') {
-        //     const futureBuyResult = await this.markets[
-        //       marketHigh
-        //     ].service.futureBuy(redisBalance, asset, aproxStableValue);
-        //     if (futureBuyResult) {
-        //       const remainingBalance = futureBuyResult.remainingBalance;
-        //       const result = futureBuyResult.futureBuyResponse;
-        //       await this.setTransactionRedis({
-        //         externalTransactionId: result.orderId,
-        //         market: marketHigh,
-        //         amountToBuy: result.origQty,
-        //         price: Number(redisBalance) - Number(remainingBalance),
-        //         asset: result.symbol,
-        //         status: result.status,
-        //         type: ActionType.FUTURE_BUY,
-        //         balanceType: 'spot',
-        //         value: remainingBalance,
-        //       });
-        //       console.log(await redisInstance.get('balance-binance-spot-usdt'));
-        //     }
-        //   }
-        // } catch (error) {
-        //   log(error);
+        //   TODO: CHECK ASSET PRICE DELTA
+        //   Get deposit network/method
+        //   const depositMethods = await this.markets[marketHigh]['getDepositMethods'](
+        //     asset,
+        //   );
+        //   // Buy Low and Future Lock High
+        //   await this.markets[marketLow]['buy'](amountToBuy, asset, aproxStableValue);
+        //   await this.markets[marketHigh]['futureBuy'](
+        //     amountToBuy,
+        //     asset,
+        //     aproxStableValue,
+        //   );
+        //   // TODO: CHECK ASSET PRICE DELTA
+        //   // Get deposit network/method
+        //   const depositMethods = await this.markets[marketHigh]['getDepositMethods'](
+        //     asset,
+        //   );
+        //   // Get transfer address
+        //   const address = await this.markets[marketHigh]['getDepositAddress'](
+        //     asset,
+        //     depositMethods[0].method,
+        //   );
+        //   // Transfer from Low to High
+        //   await this.markets[marketLow]['transfer'](
+        //     asset,
+        //     amountToBuy,
+        //     address.address,
+        //   );
+        //   // Sell High and Future Lock
+        //   await this.markets[marketHigh]['sell'](amountToBuy, asset);
+        //   await this.markets[marketHigh]['futureSell'](amountToBuy, asset);
         // }
-        // await this.markets[marketHigh]['futureBuy'](
-        //   amountToBuy,
-        //   asset,
-        //   aproxStableValue,
-        // );
-        // TODO: CHECK ASSET PRICE DELTA
-        // Get deposit network/method
-        // const depositMethods = await this.markets[marketHigh]['getDepositMethods'](
-        //   asset,
-        // );
-        // // Buy Low and Future Lock High
-        // await this.markets[marketLow]['buy'](amountToBuy, asset, aproxStableValue);
-        // await this.markets[marketHigh]['futureBuy'](
-        //   amountToBuy,
-        //   asset,
-        //   aproxStableValue,
-        // );
-        // // TODO: CHECK ASSET PRICE DELTA
-        // // Get deposit network/method
-        // const depositMethods = await this.markets[marketHigh]['getDepositMethods'](
-        //   asset,
-        // );
-        // // Get transfer address
-        // const address = await this.markets[marketHigh]['getDepositAddress'](
-        //   asset,
-        //   depositMethods[0].method,
-        // );
-        // // Transfer from Low to High
-        // await this.markets[marketLow]['transfer'](
-        //   asset,
-        //   amountToBuy,
-        //   address.address,
-        // );
-        // // Sell High and Future Lock
-        // await this.markets[marketHigh]['sell'](amountToBuy, asset);
-        // await this.markets[marketHigh]['futureSell'](amountToBuy, asset);
       }
     } catch (e) {
       console.log(e);
     }
 
     // market low to market high transaction
-    redisInstance.set(
-      {
-        key: 'TRANSFER',
-        transactionId: await this.markets[marketLow].transfer(
-          asset,
-          await this.markets[marketHigh].getDepositAddress(asset),
-        ).id,
-      },
-      300,
-    );
-
-    // TODO: CHECK ASSET PRICE DELTA
-
-    // Get deposit network/method
-    // const depositMethods = await this.markets[marketHigh]['getDepositMethods'](
-    //   asset,
+    // redisInstance.set(
+    //   {
+    //     key: 'TRANSFER',
+    //     transactionId: await this.markets[marketLow].transfer(
+    //       asset,
+    //       await this.markets[marketHigh].getDepositAddress(asset),
+    //     ).id,
+    //   },
+    //   300,
     // );
-
-    // // Buy Low and Future Lock High
-    // await this.markets[marketLow]['buy'](amountToBuy, asset, aproxStableValue);
-    // await this.markets[marketHigh]['futureBuy'](
-    //   amountToBuy,
-    //   asset,
-    //   aproxStableValue,
-    // );
-    // // TODO: CHECK ASSET PRICE DELTA
-
-    // // Get deposit network/method
-    // const depositMethods = await this.markets[marketHigh]['getDepositMethods'](
-    //   asset,
-    // );
-
-    // // Get transfer address
-    // const address = await this.markets[marketHigh]['getDepositAddress'](
-    //   asset,
-    //   depositMethods[0].method,
-    // );
-
-    // // Transfer from Low to High
-    // await this.markets[marketLow]['transfer'](
-    //   asset,
-    //   amountToBuy,
-    //   address.address,
-    // );
-    // // Sell High and Future Lock
-    // await this.markets[marketHigh]['sell'](amountToBuy, asset);
-    // await this.markets[marketHigh]['futureSell'](amountToBuy, asset);
   }
   async getMarketsBalance(
     market: string,
